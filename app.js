73f7eed14a6959b612cf2c8980a1888666788e4b
badr <badrhamidou@gmail.com>
2023-09-21 14:22:48 +0200

    add fucntion checkmail

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var email = "";
 var password = "";
 
-var usu = "admin";
+var usu = "admin@test.com";
 var pass = "1234";
 
 function checkUser() {
@@ -9,27 +9,68 @@ function checkUser() {
   password = document.getElementById("password").value;
 
   if (checkValues(email, password)) {
+    if (checkMail(email)) {
       if (email == usu && password == pass) {
-          //confirm('contraseña correcta')
-          window.location.replace("./test.html");
-        } else {
-            //alert('contraseña incorrecta')
-            document.getElementById("test").innerHTML =
-            "<h3>Contraseña incorrecta vulve a intenterlo</h3>";
-        }
-    }else {
-        document.getElementById("test").innerHTML =
-        "<h3>Campos vacíos</h3>";
+        window.location.href = "./profile.html";
+      } else {
+        document.getElementById("notificacion").innerHTML =
+          "<h4>Usuario o contraseña incorrectos vuelve a intenterlo</h4>";
+      }
+    } else {
+      document.getElementById("notificacion").innerHTML =
+        "<h4>Debe de introducir un correo válido</h4>";
     }
+  } else {
+    document.getElementById("notificacion").innerHTML =
+      "<h4>Campos vacios</h4>";
+  }
 }
 
+function checkValues(email, password) {
+  var continuar = false;
 
-function checkValues(email, password){
-    var continuar = true
+  if (email.length > 1 || password.length > 1) {
+    continuar = true;
+  }
+  return continuar;
+}
 
-    if(empty(email) || empty(password)){
-        continuar = false
+function checkMail(email) {
+  check = false;
+
+  let sim = email.split("");
+
+  let count = 0;
+  sim.forEach((value) => {
+    if (value == "@") count++;
+  });
+
+  if (count <= 1) {
+    let v = email;
+    let domain = v.slice(searchBy(email, "@") + 1, v.length);
+    let usuario = email.slice(0, searchBy(email, "@"));
+
+    //contador de puntos
+
+    var aux = 0;
+    sim.forEach((value) => {
+      if (value == ".") aux++;
+    });
+
+    if (aux >= 1) {
+      check = true;
     }
+  }
 
-    return continuar
+  return check;
+}
+
+function searchBy(email, char) {
+  let num = 0;
+  let sim = email.split("");
+  while (sim[num] != char) {
+    num++;
+  }
+
+  return num;
 }
