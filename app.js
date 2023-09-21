3a1887d137f779cd03df150c7ebf525c65652888
badr <badrhamidou@gmail.com>
2023-09-21 22:50:26 +0200

    fix checkMail()

--- app.js
+++ app.js
@@ -2,63 +2,76 @@ var email = "";
 var password = "";
 
 var usu = "Yi5oYW1pZG91QGJhZHJ3ZWIuZXM=";
-var pass = "MTIzNA==";
-var arr = "#$()|ºª~-_<+*¿?!%/&={}+`´^;\":,";
+var pass = "MTIzNDU2Nzg=";
+var arr = '#$()|ºª~-_<+*¿?!%/&={}+`´^;":,';
 var arrChars = arr.split("");
-
-
-
 
 function checkUser() {
   email = document.getElementById("email").value;
   password = document.getElementById("password").value;
-  
+
   if (checkSpecialStrings(email, arrChars)) {
     if (checkEmptyValues(email, password)) {
       if (checkMail(email)) {
-        if (email == atob(usu) && password == atob(pass)) {
-          window.location.href = "./profile.html";
+        if (checkPass(password)) {
+          if (email == atob(usu) && password == atob(pass)) {
+            window.location.href = "./profile.html";
+          } else {
+            sendNotification(
+              "notificacion",
+              "Usuario o contraseña incorrectos vuelve a intenterlo"
+            );
+          }
         } else {
           sendNotification(
             "notificacion",
-            "Usuario o contraseña incorrectos vuelve a intenterlo"
+            "Debe de tener al menos 8 caracteres la contraseña"
+
           );
         }
       } else {
-        sendNotification("notificacion", "Debe de introducir un correo válido");
+        sendNotification(
+          "notificacion",
+          "Debe de introducir un correo válido"
+        );
       }
     } else {
       sendNotification("notificacion", "Campos vacios");
     }
   } else {
-    sendNotification("notificacion", "Ha introducido caracteres no válidos en el correo");
+    sendNotification(
+      "notificacion",
+      "Ha introducido caracteres no válidos en el correo"
+    );
   }
 }
 
-
-
-
-
-
-
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function checkPass(str) {
+  var rtn = str.length >= 8;
+  console.log(rtn);
+  return rtn;
+}
+
 function checkMail(email) {
   check = false;
 
   let arrLetrs = email.split("");
 
-    let count = countChars(email, "@");
+  let count = countChars(email, "@");
 
   if (count <= 1) {
-
     let domain = email.slice(countLongBy(email, "@") + 1, email.length);
+    let tld = domain.split(".")
 
-    //comprobamos que haya más de un "." en el dominio 
-    var countDots = countChars(domain, ".")
-    if (countDots >= 1) {
+    let user = email.slice(0, countLongBy(email, "@"));
+
+    //comprobamos que haya más de un "." en el dominio
+    var countDots = countChars(domain, ".");
+    if (countDots >= 1 && user.length>=1 && tld[0].length>=1 && tld[1].length>=1) {
       check = true;
     }
-
   }
 
   return check;
@@ -70,7 +83,7 @@ function countLongBy(str, searchChar) {
   let num = 0;
   let arrStr = str.split("");
 
-  while (arrStr[num] != searchChar) {
+  while (arrStr[num] != searchChar && arrStr.length>num) {
     num++;
   }
 
@@ -78,16 +91,16 @@ function countLongBy(str, searchChar) {
 }
 
 //devuelve el número de veces que aparece searchChar en str
-function countChars(str, searchChar){
-  let count = 0
-  let i = 0
- 
-  while (str.length>i){
-    if(str[i]==searchChar) count++
-    i++
+function countChars(str, searchChar) {
+  let count = 0;
+  let i = 0;
+
+  while (str.length > i) {
+    if (str[i] == searchChar) count++;
+    i++;
   }
 
-  return count
+  return count;
 }
 
 function checkEmptyValues(email, password) {
@@ -114,7 +127,6 @@ function checkSpecialStrings(str, pattern) {
 
   while (arrStr.length > i) {
     while (pattern.length > j) {
-      
       if (arrStr[i] == pattern[j]) {
         check = false;
         break;
